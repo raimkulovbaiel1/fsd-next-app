fd9c7884c57ac67bb54e0d344ce19f208bb78d05
baiel <raimkulovbaiel2@gmail.com>
2026-01-02 21:18:19 +0600

    reaft header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Filter } from '@/widgets/Filter';
 import { Features } from '@/widgets/features'; 
-import { Header } from '@/widgets/Header';
 
 export default function Home() {
   return (
     <main className="min-h-screen"> 
-    <Header/>
+   
       <Filter />
       <Features /> 
      
