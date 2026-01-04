5e6fc59ec5022aacbc7e1da708647682f0f47476
baiel <raimkulovbaiel2@gmail.com>
2026-01-04 18:46:53 +0600

    added-commit Wishlist

---
+++ src/app/Wishlist/page.tsx
@@ -0,0 +1,11 @@
+import Favorites from "@/app/Wishlist/Sections/Favorites"
+
+const FavoritesPage  = () => {
+  return (
+    <div> 
+       <Favorites/>
+    </div>
+  )
+}
+
+export default FavoritesPage;
