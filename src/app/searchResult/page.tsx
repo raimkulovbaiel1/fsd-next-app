1dff9f4ee7c54a235bd9b8dae4f9cf2d7d49a5d7
baiel <raimkulovbaiel2@gmail.com>
2026-01-10 21:33:57 +0600

    added.

--- src/app/searchResult/page.tsx
+++ src/app/searchResult/page.tsx
@@ -1,9 +1,9 @@
 import { SearchResult } from '@/widgets/SearchResult';
 import Link from 'next/link';
 
-function SearchResultPage() {
+export default function SearchResultPage() {
   return (
-    <div className="max-w-7xl mx-auto px-2">
+    <div className="max-w-7xl mx-auto px-2 min-h-screen">
       <nav className="text-[14px] text-[#8B959E] py-4">
         <Link href="/" className="hover:text-green-500">
           Главная
@@ -11,10 +11,7 @@ function SearchResultPage() {
         <span className="mx-2">{'>'}</span>
         <span className="text-[#252525]">Транспортные средства</span>
       </nav>
-
       <SearchResult />
     </div>
   );
 }
-
-export default SearchResultPage;
