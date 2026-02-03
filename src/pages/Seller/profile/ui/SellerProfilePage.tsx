1f9d77b81ad5cbfb3221341b090a159a4226269b
baiel <raimkulovbaiel2@gmail.com>
2026-02-03 18:55:01 +0600

    added/sellerProfile

--- src/pages/Seller/profile/ui/SellerProfilePage.tsx
+++ src/pages/Seller/profile/ui/SellerProfilePage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import carbon from "@/shared/assets/img/carbon.svg";
 import { FiSearch } from "react-icons/fi";
-
+import delta from "@/shared/assets/icons/delta.svg"
 interface Vehicle {
   id: string;
   name: string;
@@ -57,14 +57,14 @@ const SellerProfilePage = () => {
 
       <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
         {vehicles.map(vehicle => (
-          <Link
+          <div
             key={vehicle.id}
-            href={`/Cart/${vehicle.id}`}
-            className="bg-white  shadow flex flex-col overflow-hidden group relative transition hover:shadow-lg"
+            className="bg-white shadow flex flex-col overflow-hidden group relative transition hover:shadow-lg"
           >
             <div className="w-full h-44 bg-gray-100 flex items-center justify-center">
               <img src={vehicle.image} alt={vehicle.name} className="object-cover w-full h-full" />
             </div>
+
             <div className="p-4 flex flex-col flex-1">
               <div className="text-[20px] mb-1 border-b">{vehicle.name}</div>
               <div className="text-gray-600 text-sm mb-2 border-b">
@@ -76,10 +76,24 @@ const SellerProfilePage = () => {
                 {vehicle.location}
               </div>
             </div>
-            <div className="absolute left-1/2 bottom-5 text-[13px] px-3 py-1 bg-[#4689661A] text-[#009661] rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition pointer-events-none">
-              Больше информации
+
+            <div className="absolute left-1/2 bottom-2 transform -translate-x-1/4 flex gap-2 text-[15px] px-3 py-2 text-[#009661] opacity-0 group-hover:opacity-100 transition pointer-events-auto">
+              <a
+                href={`/edit/${vehicle.id}`}
+                className="px-3 py-1 bg-white text-[#009661] rounded-lg shadow"
+              >
+                Редактировать
+              </a>
+              <a
+                href={`/delete/${vehicle.id}`}
+                className="flex items-center gap-2 px-4 py-1 bg-[#EB57571A] text-[#EB5757] rounded-lg shadow"
+              >
+                <img src={delta.src} alt="logo" className="w-4 h-4" />
+                Удалить
+              </a>
+
             </div>
-          </Link>
+          </div>
         ))}
 
         <div className="flex justify-center mt-6 w-full">
@@ -88,6 +102,7 @@ const SellerProfilePage = () => {
           </button>
         </div>
       </div>
+
     </div>
   );
 };
