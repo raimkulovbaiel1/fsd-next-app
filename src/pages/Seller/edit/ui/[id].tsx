6cbf53048b55da64eadb28f8af02de88165c5683
baiel <raimkulovbaiel2@gmail.com>
2026-02-06 03:11:10 +0600

    added/edit

--- src/pages/Seller/edit/ui/[id].tsx
+++ src/pages/Seller/edit/ui/[id].tsx
@@ -10,7 +10,6 @@ export default function EditPage() {
   return (
     <div className="max-w-6xl mx-auto p-4 bg-white min-h-screen">
 
-      {/* TOP BAR */}
       <div className="flex items-center justify-between mb-4">
         <Link href="/seller/profile" className="text-sm text-gray-400 hover:underline">
           ← Назад
@@ -26,10 +25,8 @@ export default function EditPage() {
         </div>
       </div>
 
-      {/* MAIN */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
 
-        {/* LEFT */}
         <div className="lg:col-span-2">
           <img
             src="https://im.mashina.kg/tachka/images//6/8/c/68c1531dc157e268707cad27b4df5e88_240x180.jpg"
@@ -37,7 +34,6 @@ export default function EditPage() {
             className="rounded-lg w-full object-cover"
           />
 
-          {/* thumbnails */}
           <div className="flex gap-3 mt-4">
             {[1, 2, 3, 4].map((i) => (
               <div
@@ -58,7 +54,6 @@ export default function EditPage() {
           </div>
         </div>
 
-        {/* RIGHT */}
         <div className="bg-white">
           <h1 className="text-lg font-semibold mb-1">
             Nissan Eco T100 Paardenvervoer
@@ -89,7 +84,6 @@ export default function EditPage() {
         </div>
       </div>
 
-      {/* SPECS */}
       <div className="mt-10">
         <h2 className="text-lg font-semibold mb-4">
           Обзор транспортного средства
